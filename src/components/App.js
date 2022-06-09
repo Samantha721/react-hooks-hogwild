68901659fefca1999868b9271129453f16ca165f
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-06-09 10:01:15 -0700

    inital import

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Nav from "./Nav";
+import DisplayTiles from "./DisplayTiles"
+import Filter from "./Filter"
+import ItemList from "./ItemList"
+
+
 
 import hogs from "../porkers_data";
 
